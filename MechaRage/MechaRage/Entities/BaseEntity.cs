de836499af06484f92d7900e87df4e0d07a45e8e
ShockThunder <skysidtim@gmail.com>
2020-10-17 22:19:29 +0700

    add base entity

--- MechaRage/MechaRage/Entities/BaseEntity.cs
+++ MechaRage/MechaRage/Entities/BaseEntity.cs
@@ -1,7 +1,38 @@
 namespace MechaRage.Entities
 {
-    public class BaseEntity
+    using Microsoft.Xna.Framework;
+    using Microsoft.Xna.Framework.Graphics;
+
+    public abstract class BaseEntity
     {
+        protected Texture2D Texture;
+        protected Color Color = Color.White;
+
+        public Vector2 Position;
+        public Vector2 Velocity;
+        public float Orientation;
         
+        /// <summary>
+        /// Radius for collision detection
+        /// </summary>
+        public float Radius;
+        
+        public bool IsDestroyed;
+
+        public Vector2 Size
+        {
+            get
+            {
+                return Texture == null ? Vector2.Zero : new Vector2(Texture.Width, Texture.Height);
+            }
+        }
+
+        public abstract void Update();
+
+        public virtual void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(Texture, Position, null, Color, Orientation, Size / 2f, 1f, 0, 0);
+        }
+
     }
 }
